2bc7f83e4d1cc8937f2e2f7d4386bca46d5baa82
Princerey <rohkumar0126@gmail.com>
2023-03-06 02:44:47 +0530

    trusted source added

--- level1/settings.py
+++ level1/settings.py
@@ -14,10 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CSRF_TRUSTED_ORIGINS = ['https://escapezaenigma-production.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://escapezaenigma-production.up.railway.app/'
-]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
